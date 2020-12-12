aafaa068381a9c4dc9fcdbf61303306fd2b9d6dc
ERodzB <honduras.eric@gmail.com>
2020-12-12 03:29:51 +0000

    Adding internet gateway facing internet and fixes to key pair

---
+++ aws_internet_gateway.tf
@@ -0,0 +1,20 @@
+resource "aws_internet_gateway" "devops_ec2_gateway" {
+    vpc_id = aws_vpc.devops_vpc.id
+}
+
+resource "aws_route_table" "devops_routing_table" {
+    vpc_id = aws_vpc.devops_vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.devops_ec2_gateway.id
+    }
+}
+
+resource "aws_route_table_association" "devops_routing_association_a" {
+    subnet_id = aws_subnet.devops_subnet_1.id
+    route_table_id = aws_route_table.devops_routing_table.id
+}
+resource "aws_route_table_association" "devops_routing_association_b" {
+    subnet_id = aws_subnet.devops_subnet_2.id
+    route_table_id = aws_route_table.devops_routing_table.id
+}
--- aws_key_pair.tf
+++ aws_key_pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "devops_key" {
   key_name   = "DevOpsKey"
-  public_key = var.AWS_KEY_PAIR["public_key"]
+  public_key = file(var.AWS_KEY_PAIR["public_key"])
 }
